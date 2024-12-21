9604d85b5b44363441771c4c99bbe33aa2bdf44b
paulwu <paul.r.wu@gmail.com>
2024-12-21 17:30:31 +0800

    第十五章作业提交

--- src/main.py
+++ src/main.py
@@ -7,22 +7,24 @@ def main():
     input_text = """
     # ChatPPT_Demo
 
-    ## ChatPPT Demo [Title Only]
+    ## 年终总结 [Title]
 
-    ## 2024 业绩概述 [Title and Content]
+    ## 2024 业绩概述 [Agenda]
     - 总收入增长15%
     - 市场份额扩大至30%
 
-    ## 业绩图表 [Title and Picture 1]
+    ## 业绩图表 [Summary]
+    - 业绩翻翻
+    - 上市指日可待
     ![业绩图表](images/performance_chart.png)
 
-    ## 新产品发布 [Title and 2 Column]
+    ## 新产品发布 [Summary]
     - 产品A: 特色功能介绍
     - 产品B: 市场定位
     ![未来增长](images/forecast.png)
     """
 
-    template_file = 'templates/MasterTemplate.pptx'
+    template_file = 'templates/Fair frames presentation.pptx'
     prs = load_template(template_file)
 
     print("Available Slide Layouts:")
@@ -31,6 +33,8 @@ def main():
     layout_mapping = get_layout_mapping(prs)
 
     powerpoint_data, presentation_title = parse_input_text(input_text, layout_mapping)
+    print(powerpoint_data)
+    print(presentation_title)
 
     output_pptx = f"outputs/{presentation_title}.pptx"
     generate_presentation(powerpoint_data, template_file, output_pptx)
